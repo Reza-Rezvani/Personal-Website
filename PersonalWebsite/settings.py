b3bcc4542ca3b1758e3709b3d9353cda2c0527fa
Reza-Rezvani <rezvanim740@gmail.com>
2023-05-19 17:54:23 +0330

    added general template folder
    moved website templates to general folder of templates

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PersonalWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
