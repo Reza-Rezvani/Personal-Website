e8c6765b74837edcd1bfd36360c27202974e4f1a
Reza-Rezvani <rezvanim740@gmail.com>
2023-05-25 09:56:32 +0330

    added the template to project
    added the template to project and matching the two

--- PersonalWebsite/urls.py
+++ PersonalWebsite/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
