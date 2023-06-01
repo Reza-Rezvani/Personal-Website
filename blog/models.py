38c527b7bbb683f6d1f203240d883539d7b96df7
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-01 16:21:37 +0330

    created database and added first data
    created database and added first data
    and migrate that

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # image
+    # # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # category 
+    # tag
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+   
