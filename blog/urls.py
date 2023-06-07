2082b070c64755bbc7819b1a51806b4313062065
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-07 18:20:46 +0330

    project update and blog modification
    project update and blog modification

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',blog_view,name='index'),
-    path('single',blog_single,name='single'),
-    path('test',test,name='test'),
+    path('<int:pid>',blog_single,name='single'),
+    # path('post-<int:pid>',test,name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
@@ -7,9 +7,13 @@ def blog_view(request):
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(posts, pk=pid, status=1)
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
 
-def test(request):
-
-    return render(request, 'test.html')
+def test(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post': post}
+    return render(request, 'test.html', context)
