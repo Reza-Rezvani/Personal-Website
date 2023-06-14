aac0fd3fd285bb4e79490a427aff44e5f415bda7
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-14 17:20:22 +0330

    the end popular post and categories
    the end popular post and categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library()
 
@@ -17,7 +18,17 @@ def function():
 def snippet(value,arg=20):
     return value[:arg] + "..."
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_date')[:1]
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count
+    return {'categories':cat_dict}
