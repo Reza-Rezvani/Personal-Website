9e9f3709ef2bf05f50595120d4892d1c75a4c6bf
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-19 19:00:57 +0330

    project updating
    project updating

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('',blog_view,name='index'),
     path('<int:pid>',blog_single,name='single'),
+    path('category/<str:cat_name>',blog_view,name='category'),
+    path('author/<str:author_username>',blog_view,name='author'),
     path('test',test,name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+    if author_username:
+        posts = posts.filter(author__username = author_username)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -15,3 +19,11 @@ def blog_single(request, pid):
 
 def test(request):
     return render(request, 'test.html')
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+
