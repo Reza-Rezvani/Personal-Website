068954e6815081cae8d1e647760b28cede377680
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-20 18:55:33 +0330

    create pagination
    create pagination (posts)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
 def blog_view(request, cat_name=None, author_username=None):
@@ -8,8 +9,19 @@ def blog_view(request, cat_name=None, author_username=None):
         posts = posts.filter(category__name=cat_name)
     if author_username:
         posts = posts.filter(author__username = author_username)
+
+    posts = Paginator(posts, 1)  
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
+    
 
 def blog_single(request, pid):
     posts = Post.objects.filter(status=1)
