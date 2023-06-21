14b763c15383b95e4c9943838b7f608da87f11c3
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-21 18:42:58 +0330

    create latest post in home page
    create latest post in home page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from blog.models import Post
 
 def index_view(request):
-    return render(request, 'website/index.html')
+    posts = Post.objects.all().order_by('-created_date')
+    context = {'posts':posts}
+    return render(request, 'website/index.html', context)
 
 
 def about_view(request):
