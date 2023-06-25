843741c02e3f853859e3f8d26b052c39b5dc0d37
Reza-Rezvani <rezvanim740@gmail.com>
2023-06-25 17:42:48 +0330

    created contact form
    created contact form

--- website/urls.py
+++ website/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('',index_view,name='index'),
     path('about',about_view,name='about'),
     path('contact',contact_view,name='contact'),
+    path('test',test_view,name='test'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from blog.models import Post
-
+from website.models import Contact
+from website.forms import NameForm,ContactForm
+from django.contrib import messages
 def index_view(request):
     posts = Post.objects.all().order_by('-created_date')
     context = {'posts':posts}
@@ -13,4 +15,25 @@ def about_view(request):
 
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited successfull')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket didnt submited')
+    form = ContactForm()
+    return render(request, 'website/contact.html',{'form':form})
+
+def test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
+
+    form = ContactForm()
+
+    return render(request,'test.html',{'form':form})
