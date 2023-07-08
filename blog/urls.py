dfa7d9dbed4b8e815333b58aed013cf7dae15448
Reza-Rezvani <rezvanim740@gmail.com>
2023-07-08 18:37:48 +0330

    added rss feed
    added rss feed to project

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from blog.views import *
+from blog.feeds import LatestEntriesFeed
 
 app_name = 'blog'
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('author/<str:author_username>',blog_view,name='author'),
     path('search/',blog_search,name='search'),
     path('test',test,name='test'),
+    path("rss/feed/", LatestEntriesFeed()),
 ]
